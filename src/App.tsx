4c2a0fe9030c31722c0ec4661c5616d547279c76
agnius <agnius@agnius.lt>
2020-07-07 22:35:20 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import LunarDatePicker from './DatePicker';
 import './App.css';
 
+const rootImgPath: string = "https://svs.gsfc.nasa.gov/vis/a000000/a004700/a004768/frames/730x730_1x1_30p/";
+
 function App() {
+  const [date, changeDate] = useState(new Date());
+
+  let imgNo = getImageNo(date);
+
+  let handleDateChange = (newDate: Date | null) : void => {
+    changeDate(newDate!);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LunarDatePicker date={date} onDateChange={handleDateChange}/>
+      <img id='moon_image' src={`${rootImgPath}moon.${imgNo}.jpg`} alt=""></img>
     </div>
   );
+}
+
+function getImageNo(currentDate : Date) : number {
+  let januaryOne = Date.UTC(currentDate.getUTCFullYear(), 0, 1, 0, 0, 0);
+  let imageNo = 1 + Math.round((currentDate.getTime() - januaryOne)/3600000.0);
+  return imageNo;
 }
 
 export default App;
